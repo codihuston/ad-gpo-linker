b98d2ab292fd8cce206e258c745521ad6757ba69
Huston <codi.huston@wku.edu>
2020-04-10 17:22:21 -0500

    init
    Features:
    - can select multiple GPOs and see where they are linked
    - can filter them
    - can clear checked gpos
    - can can reset the filter
    - can open the AD OU/container selection window; displays all
    OUs/sites/containers, but does not format it nicely yet
    - has about window (no gitlab link yet)
    
    TODO:
    - format AD OU structure in an alphabetized, recursive/nested manner
    - after selecting OUs, apply link them to the given OUs
    - should prevent opening the AD window if no policies are selected

---
+++ Globals.ps1
@@ -0,0 +1,109 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+
+function Add-ToTreeView
+{
+	[cmdletbinding()]
+	Param (
+		[Parameter(Mandatory = $true)]
+		[System.Array]$ArrayOfObjects,
+		[Parameter(Mandatory = $true)]
+		[System.String]$DisplayMember,
+		[Parameter(Mandatory = $false)]
+		[System.Object]$DisplayFunction,
+		[Parameter(Mandatory = $true)]
+		[System.Windows.Forms.TreeView]$TreeView,
+		[Parameter(Mandatory = $true)]
+		[System.Collections.Hashtable]$CheckedNodes,
+		[Parameter(Mandatory = $false)]
+		[System.String]$Filter,
+		[Parameter(Mandatory = $false)]
+		[System.String]$MemberToFilterOn
+	)
+	# for each gpo
+	$ArrayOfObjects | %{
+		if ($DisplayFunction)
+		{
+			# don't forget the quotes around displaymember so powershell doesn't convert it into an array by default (fn expects string))
+			$name = (Invoke-Expression "$DisplayFunction '$($_.$DisplayMember)'")
+			
+			#Write-Debug ("Invoke: $DisplayFunction $($_.$DisplayMember)")
+			#Write-Debug "`t yielded: $name"
+			
+			$node = New-Object -TypeName System.Windows.Forms.TreeNode -ArgumentList "$name"
+		}
+		else
+		{
+			$node = New-Object -TypeName System.Windows.Forms.TreeNode -ArgumentList $_.$DisplayMember
+		}
+		$node.Tag = $_
+		
+		# if it was previously in the checked items list, check it again (if re-adding the node to the tree...)
+		if ($CheckedNodes[$_.Id])
+		{
+			$node.Checked = $true
+		}
+		
+		# add it to the tree view
+		if ($Filter)
+		{
+			Write-Debug "Match $($_.$MemberToFilterOn) -like $Filter"
+			if ($_.$MemberToFilterOn -like "*$Filter*")
+			{
+				$TreeView.Nodes.Add($node)
+			}
+		}
+		else
+		{
+			$TreeView.Nodes.Add($node)
+		}
+	}
+}
+
+function Get-CanonicalName ([string[]]$DistinguishedName)
+{
+	foreach ($dn in $DistinguishedName)
+	{
+		$d = $dn.Split(',') ## Split the dn string up into it's constituent parts 
+		$arr = (@(($d | Where-Object { $_ -notmatch 'DC=' }) | ForEach-Object { $_.Substring(3) })) ## get parts excluding the parts relevant to the FQDN and trim off the dn syntax 
+		[array]::Reverse($arr) ## Flip the order of the array. 
+		
+		## Create and return the string representation in canonical name format of the supplied DN 
+		"{0}/{1}" -f (($d | Where-Object { $_ -match 'dc=' } | ForEach-Object { $_.Replace('DC=', '') }) -join '.'), ($arr -join '/')
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+$script:version = "v0.1"
+
+
